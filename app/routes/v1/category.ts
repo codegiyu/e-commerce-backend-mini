fae49b900cbf8c534102d2bbb4b1355b45c775a7
Abiola Zeenat Abdulsalam <81884414+Abiola-Zeenat@users.noreply.github.com>
2025-01-02 15:40:27 +0100

    refactor

--- app/routes/v1/category.ts
+++ app/routes/v1/category.ts
@@ -1,18 +1,18 @@
-import express, { Router } from "express";
+import express, { Router } from 'express';
 import {
   createCategory,
   updateCategory,
   deleteCategory,
   getAllCategories,
   getSpecificCategory,
-} from "../../controllers/category";
-import { admin } from "../../middlewares/authMiddleware";
-import { handleValidation } from "../../controllers/category/create";
+} from '../../controllers/category';
+import { admin } from '../../middlewares/authMiddleware';
+import { handleValidation } from '../../controllers/category/create';
 
 export const router: Router = express.Router();
 
-router.post("/create", admin, handleValidation, createCategory);
-router.put("/update/:id", admin, handleValidation, updateCategory);
-router.delete("/delete/:id", admin, deleteCategory);
-router.get("/all", getAllCategories);
-router.get("/:id", getSpecificCategory);
+router.post('/create', admin, handleValidation, createCategory);
+router.put('/:id', admin, handleValidation, updateCategory);
+router.delete('/:id', admin, deleteCategory);
+router.get('/all', getAllCategories);
+router.get('/:id', getSpecificCategory);
